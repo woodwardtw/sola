c8d5e5c51a8696f743f17c55971ff6e6c4ec8868
Tom Woodward <twwoodward@me.com>
2020-02-28 21:18:59 -0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site <?php sola_is_home();?>" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div id="wrapper-navbar">
+
+		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+
+		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+
+			<h2 id="main-nav-label" class="sr-only">
+				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+			</h2>
+
+		<?php if ( 'container' === $container ) : ?>
+			<div class="container">
+		<?php endif; ?>
+
+			<!-- Your site title as branding in the menu -->
+			<div class="title-menu">
+				<img src="<?php echo get_template_directory_uri() . '/imgs/sola.png" class="sun-menu" alt="Karlstad icon."';?>">
+				    <div class="menu-text">
+						<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+					    <div class="menu-description"><?php bloginfo('description');?></div>
+					</div>
+			</div>
+
+
+			<!-- end custom logo -->
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<!-- The WordPress Menu goes here -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+			<?php if ( 'container' === $container ) : ?>
+			</div><!-- .container -->
+			<?php endif; ?>
+
+		</nav><!-- .site-navigation -->
+
+	</div><!-- #wrapper-navbar end -->
+		<?php echo get_the_post_thumbnail( $post->ID, 'full' ); ?>
+
---
+++ inc/acf.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ACF
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+  
+ //ACF JSON SAVER
+  add_filter('acf/settings/save_json', 'sola_json_save_point');
+   
+  function sola_json_save_point( $path ) {
+      
+      // update path
+      $path = get_stylesheet_directory() . '/acf-json';
+      
+      // return
+      return $path;
+      
+  }
+
+  //ACF JSON LOADER
+  add_filter('acf/settings/load_json', 'sola_acf_json_load_point');
+
+  function sola_acf_json_load_point( $paths ) {
+      
+      // remove original path (optional)
+      unset($paths[0]);    
+      
+      // append path
+      $path = get_stylesheet_directory() . '/acf-json';
+      
+      // return
+      return $paths;
+      
+  }
+
+  function sola_icon_loop(){
+  	global $post;
+  	if( have_rows('icon_blocks', $post->ID) ):
+
+ 	// loop through the rows of data
+    while ( have_rows('icon_blocks') ) : the_row();
+
+        // display a sub field value
+        echo '<div class="col-md-4 icon-block">';
+        echo '<img src="' . get_sub_field('icon')['sizes']['thumbnail'] . '" class="icon-img">';
+        echo '<h2>' . get_sub_field('block_title') . '</h2>';
+        echo '<p>' . get_sub_field('block_text') . '</p>';
+        echo '</div>';
+
+    endwhile;
+
+	else :
+
+	    // no rows found
+
+	endif;
+
+
+  }
